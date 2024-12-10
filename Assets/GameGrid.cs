bbc575457c0ec433ad874175565775b97c756b34
gangrel2321 <jared.spang@columbia.edu>
2024-12-10 01:37:07 -0500

    commit current version, cube roles around, custom shader applied, no coloring

---
+++ Assets/GameGrid.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+
+    private int height = 4;
+    private int width = 4;
+    private float Spacing = 1.0f;
+
+    [SerializeField] private GameObject gridCellPrefab;
+    [SerializeField] private GameObject playerPrefab;
+    private GameObject[,] gameGrid;
+    private GameObject player; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void CreateGrid()
+    {
+        gameGrid = new GameObject[height, width];
+        player = new GameObject();
+        if (gridCellPrefab == null)
+        {
+            Debug.LogError("Error: Grid Cell Prefab is not assigned");
+            return;
+        }
+        if (playerPrefab == null)
+        {
+            Debug.LogError("Error: Player Prefab is not assigned");
+            return;
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                gameGrid[x, y] = Instantiate(gridCellPrefab, new Vector3(x * Spacing, 0, y * Spacing), Quaternion.identity);
+                gameGrid[x, y].transform.parent = transform;
+                gameGrid[x, y].gameObject.name = "Grid Space ( x: " + x.ToString() + " , z: " + y.ToString() + " )\n";
+            }
+        }
+        float p_x = Random.Range(0, width) * Spacing;
+        float p_y = Random.Range(0, height) * Spacing;
+        float p_z = Spacing;
+        player = Instantiate(playerPrefab, new Vector3(p_x, p_z, p_y), Quaternion.identity);
+        player.transform.parent = transform;
+        player.gameObject.name = "Player";
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _rollSpeed = 8;
+    private bool _isMoving;
+
+    void Update()
+    {
+        if (_isMoving) return;
+
+        if (Input.GetKeyDown(KeyCode.A)) Assemble(Vector3.left);
+        if (Input.GetKeyDown(KeyCode.W)) Assemble(Vector3.forward);
+        if (Input.GetKeyDown(KeyCode.S)) Assemble(Vector3.back);
+        if (Input.GetKeyDown(KeyCode.D)) Assemble(Vector3.right);
+
+
+        void Assemble(Vector3 direction)
+        {
+            var anchor = transform.position + (Vector3.down + direction) * 0.5f;
+            var axis = Vector3.Cross(Vector3.up, direction);
+            StartCoroutine(Roll(anchor, axis));
+        }
+    }
+
+    IEnumerator Roll(Vector3 anchor, Vector3 axis)
+    {
+        _isMoving = true;
+
+        for (int i = 0; i < (90 / _rollSpeed); i++)
+        {
+            transform.RotateAround(anchor, axis, _rollSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        _isMoving = false;
+    }
+}
